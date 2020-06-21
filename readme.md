321d379c2c9b5581b5e9ac0f39477911e71be782
caffeinateddev0 <caffeinateddev0@gmail.com>
2020-06-20 20:20:45 -0700

    feat(context): added create data context

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Simple UI
+
+## Description
+Simple UI is a collection of commonly used react components with easily
+updated and managed styling.
+
+## Peer Dependencies
+- [https://styled-components.com/] styled components
+- [https://reactjs.org/] react, react-dom
+
---
+++ src/helpers/createDataContext.tsx
@@ -0,0 +1,57 @@
+import React, { useReducer } from 'react';
+
+export interface CreateDataContextActionProps {
+  type: string;
+  payload: any;
+}
+
+interface CreateDataContextProps {
+  reducer: (state: any, action: CreateDataContextActionProps) => void;
+  actions: {
+    [key: string]: (
+      dispatch: ({
+        type,
+        payload,
+      }: CreateDataContextActionProps) => void,
+    ) => (props: any) => void;
+  };
+  initalState: any;
+}
+
+interface ProviderProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+/**
+ * @summary createDataContext: Uses react context to pass state using a redux style action / reducer
+ * @param { reducer, actions, initialState }: Initial object containing the reducer, actions, and initialState
+ * @param { state: any, action } reducer: Function that receives the current state as the first
+ * prop and incoming action as the second
+ * @param { object } actions = object containing avaliable actions
+ * @param { any } initalState = inital state provided to the reducer
+ */
+export const createDataContext = ({
+  reducer,
+  actions,
+  initalState,
+}: CreateDataContextProps) => {
+  const Context = React.createContext({});
+
+  const Provider = ({ children }: ProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initalState);
+
+    const boundActions: { [key: string]: (props: any) => void } = {};
+
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
+
+    return (
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
+    );
+  };
+
+  return { Context, Provider };
+};
